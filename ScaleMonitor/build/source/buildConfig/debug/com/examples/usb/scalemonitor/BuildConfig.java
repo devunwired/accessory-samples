91975aa42bdb8e71db2468ad950f75e4c5b40369
Dave Smith <smith@wiresareobsolete.com>
2013-08-26 22:05:35 -0600

    Update build files and ignore files.

--- ScaleMonitor/build/source/buildConfig/debug/com/examples/usb/scalemonitor/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.examples.usb.scalemonitor;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
--- UsbMonitor/build/source/buildConfig/debug/com/example/UsbMonitor/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.UsbMonitor;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-
-}
